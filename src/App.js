a6a3b07e9d9d37aa6fa2036e436081c09830f464
MCH <111715559+mchxyz@users.noreply.github.com>
2024-08-14 11:31:29 -0400

    Update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import './App.css';
 import WeatherSearch from "./WeatherSearch";
-// import Time from "./Time";
 
 export default function App() {
   return (
     <div className="App-Container">
       <header className="Weather-Container">
-       {/* <Time /> */}
        <WeatherSearch />
       </header>
     </div>
