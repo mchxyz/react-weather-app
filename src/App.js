4f38e5786cb1ede72f48bbef5f52c59874b058f1
MCH <111715559+mchxyz@users.noreply.github.com>
2024-08-14 11:29:20 -0400

    Grayed out time

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import React from "react";
-import Weather from "./Weather";
 import './App.css';
 import WeatherSearch from "./WeatherSearch";
-import Time from "./Time";
+// import Time from "./Time";
 
 export default function App() {
   return (
     <div className="App-Container">
       <header className="Weather-Container">
-        {/* <h1>
-        Hello from React
-        </h1>
-       <Weather city="Tokyo" /> */}
-       <Time />
+       {/* <Time /> */}
        <WeatherSearch />
       </header>
     </div>
--- src/Time.js
+++ src/Time.js
@@ -1,36 +1,34 @@
-// import React, { useState } from 'react';
-
-import React,{ useEffect, useRef, useState } from "react";
+// import React,{ useEffect, useRef, useState } from "react";
 
 
-export default function App() {
-  let newDate = useRef(new Date());
+// export default function App() {
+//   let newDate = useRef(new Date());
 
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+//   let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-  let [currentDay, setCurrentDay] = useState(days[newDate.current.getDay()]);
-  let [currentHours, setCurrentHours] = useState(newDate.current.getHours());
-  let [currentMinutes, setCurrentMinutes] = useState(newDate.current.getMinutes());
-  let [currentSeconds, setCurrentSeconds] = useState(newDate.current.getSeconds());
+//   let [currentDay, setCurrentDay] = useState(days[newDate.current.getDay()]);
+//   let [currentHours, setCurrentHours] = useState(newDate.current.getHours());
+//   let [currentMinutes, setCurrentMinutes] = useState(newDate.current.getMinutes());
+//   let [currentSeconds, setCurrentSeconds] = useState(newDate.current.getSeconds());
 
-  setInterval(() => {
-    newDate.current = new Date();
-    setCurrentHours(newDate.current.getHours());
-    setCurrentMinutes(newDate.current.getMinutes());
-  }, 1000);
-  let currentTime = `${currentDay} ${currentHours}:${currentMinutes}`;
-  useEffect(() => {
-  });
+//   setInterval(() => {
+//     newDate.current = new Date();
+//     setCurrentHours(newDate.current.getHours());
+//     setCurrentMinutes(newDate.current.getMinutes());
+//   }, 1000);
+//   let currentTime = `${currentDay} ${currentHours}:${currentMinutes}`;
+//   useEffect(() => {
+//   });
 
-  if (currentMinutes < 10) {
-    return currentDay + " " + currentHours + ":" + "0" + currentMinutes;
-  } else {
-    return currentDay + " " + currentHours + ":" + currentMinutes;
-  }
+//   if (currentMinutes < 10) {
+//     return currentDay + " " + currentHours + ":" + "0" + currentMinutes;
+//   } else {
+//     return currentDay + " " + currentHours + ":" + currentMinutes;
+//   }
 
-  return (
-    <div>
-      <div>{currentTime}</div>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <div>{currentTime}</div>
+//     </div>
+//   );
+// }
